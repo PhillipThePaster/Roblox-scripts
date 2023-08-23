ae4fec02df09117843caeae34fd9351f94b459bc
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-08-22 21:45:45 -0400

    Create Thirdperson.lua

---
+++ misc/Thirdperson.lua
@@ -0,0 +1,12 @@
+--wont work in all games
+local LocalPlayer = game.Players.LocalPlayer
+local CurrentCamera = workspace.CurrentCamera
+
+local newindex; newindex = hookmetamethod(game, '__newindex', function(obj, idx, val)
+
+   if obj == CurrentCamera and idx == 'CFrame' then
+       val = val + (val.LookVector * -1)
+   end
+
+   return newindex(obj, idx, val)
+end)
