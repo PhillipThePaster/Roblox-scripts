8fbfed3b45ace52b46ca28d047c0480455a4f8e2
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-08-22 21:43:40 -0400

    Create ChatPrintfunction.lua

---
+++ misc/ChatPrintfunction.lua
@@ -0,0 +1,3 @@
+--this prints any text you want in chat that only you can see
+--works on most games not all
+game.StarterGui:SetCore( "ChatMakeSystemMessage",  { Text = "Example", Color = Color3.fromRGB( 255, 3, 125 ), Font = Enum.Font.Arial, FontSize = Enum.FontSize.Size24 } )
